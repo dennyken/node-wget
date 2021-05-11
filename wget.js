804dbd68d7cb243ede34ecfb457a67c11a82e722
Denny Ken <denny.kmm@gmail.com>
2021-05-11 01:34:41 -0300

    include url lib to assert valid url

--- wget.js
+++ wget.js
@@ -1,10 +1,19 @@
+const url = require('url')
 const https = require('https')
 const fs = require('fs')
 
 const flags = process.argv.filter(arg => arg.startsWith('-'))
-let url = process.argv.slice(2).find(arg => !arg.startsWith('-'))
+let input = process.argv.slice(2).find(arg => !arg.startsWith('-'))
 
-https.get(url, (res) => {
+try {
+  input = new URL(input)
+}
+catch (err) {
+  console.error('Error: Invalid URL')
+}
+
+
+https.get(input, (res) => {
   let data = ''
 
   res.on('data', (chunk) => {
@@ -12,7 +21,7 @@ https.get(url, (res) => {
   })
 
   res.on('end', () => {
-    const fileName = url.slice(url.lastIndexOf('/') + 1)
+    const fileName = input.slice(input.lastIndexOf('/') + 1)
 
     fs.writeFile(fileName, data, (err) => {
       if (err) console.log(err)
