76bc89aef585c96a0da80d7a762dbf4a9066e293
Denny Ken <denny.kmm@gmail.com>
2021-05-15 00:01:39 -0300

    create getAdapter and fetchData functions

--- resources/web.js
+++ resources/web.js
@@ -1,8 +1,30 @@
+getAdapter = (protocol) => {
+  const adapters = {
+    'http:': require('http'),
+    'https:': require('https')
+  }
+
+  return adapters[protocol]
+}
+
+exports.fetchData = (url) => {
+  getAdapter(url.protocol).get(url.href, (res) => {
+    let data = ''
+  
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+  
+    res.on('end', () => {  
+      return data
+    })
+  }).on('error', err => console.log(err))
+}
+
 exports.parseUrl = (url) => {
   try {
     return new URL(url)
-  }
-  catch (err) {
+  } catch (err) {
     process.stderr.write('Error: Invalid URL')
   }
 
--- wget.js
+++ wget.js
@@ -1,21 +1,8 @@
-const https = require('https')
 const { writeFile, getFileName } = require('./resources/file')
-const { parseUrl } = require('./resources/web')
+const { parseUrl, fetchData } = require('./resources/web')
 
 const flags = process.argv.filter(arg => arg.startsWith('-'))
 let input = process.argv.slice(2).find(arg => !arg.startsWith('-'))
 const url = parseUrl(input)
 
-if(url.href) {
-  https.get(url.href, (res) => {
-    let data = ''
-  
-    res.on('data', (chunk) => {
-      data += chunk
-    })
-  
-    res.on('end', () => {  
-      writeFile(getFileName(url), data)
-    })
-  }).on('error', err => console.log(err))
-}
+writeFile(getFileName(url), fetchData(url))
