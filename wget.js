05a1ab60bf8aa349559e521dc2817318f23265ed
Denny Ken <denny.kmm@gmail.com>
2021-05-16 16:33:29 -0300

    implement getting multiple files in a single call

--- wget.js
+++ wget.js
@@ -1,8 +1,19 @@
 const { writeFile, getFileName } = require('./resources/file')
 const { parseUrl, fetchData } = require('./resources/web')
 
-const flags = process.argv.filter(arg => arg.startsWith('-'))
-let input = process.argv.slice(2).find(arg => !arg.startsWith('-'))
-const url = parseUrl(input)
+const [nodePath, mainFile, ...args] = process.argv
 
-writeFile(getFileName(url), fetchData(url))
+const [flags, inputs] = (() => {
+  const flags = []
+  const inputs = []
+  
+  args.map(arg => {
+    arg.startsWith('-') ? flags.push(arg) : inputs.push(arg)
+  })
+
+  return [flags, inputs]
+})()
+
+const urls = inputs.map(input => parseUrl(input))
+
+urls.map(url => writeFile(getFileName(url), fetchData(url)))
